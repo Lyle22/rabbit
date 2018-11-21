9793420c6164a7313272ceeaf45cfa94bbeebcba
郑正光 <zhengzhengguang@369exhibition.com>
2018-11-21 14:27:50 +0800

    fisrt commit

---
+++ rabbit-parent/front/src/main/java/org/rabbit/front/App.java
@@ -0,0 +1,13 @@
+package org.rabbit.front;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
