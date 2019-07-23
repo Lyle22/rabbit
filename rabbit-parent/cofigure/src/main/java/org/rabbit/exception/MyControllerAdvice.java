ad07d9208ad7c1a9a5b348ad2465a3902e34e77b
郑正光 <zhengzhengguang@369exhibition.com>
2019-07-23 18:19:40 +0800

    全局异常处理

---
+++ rabbit-parent/cofigure/src/main/java/org/rabbit/exception/MyControllerAdvice.java
@@ -0,0 +1,61 @@
+package org.rabbit.exception;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import org.springframework.ui.Model;
+import org.springframework.web.bind.WebDataBinder;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.InitBinder;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@ControllerAdvice
+public class MyControllerAdvice {
+	
+	/**
+     * 应用到所有@RequestMapping注解方法，在其执行之前初始化数据绑定器
+     * @param binder
+     */
+    @InitBinder
+    public void initBinder(WebDataBinder binder) {}
+
+    /**
+     * 把值绑定到Model中，使全局@RequestMapping可以获取到该值
+     * @param model
+     */
+    @ModelAttribute
+    public void addAttributes(Model model) {
+        model.addAttribute("user", "zzg");
+    }
+
+    /**
+     * 全局异常捕捉处理
+     * @param ex
+     * @return
+     */
+    @ResponseBody
+    @ExceptionHandler(value = Exception.class)
+    public Map<String, Object> errorHandler(Exception ex) {
+        Map<String, Object> map = new HashMap<String, Object>();
+        map.put("code", 10001);
+        map.put("msg", ex.getMessage());
+        return map;
+    }
+    
+    /**
+     * 全局自定义异常捕捉处理
+     * @param ex
+     * @return
+     */
+    @ResponseBody
+    @ExceptionHandler(value = BaseException.class)
+    public Map<String, Object> defaultHandler(BaseException e) {
+        Map<String, Object> map = new HashMap<String, Object>();
+        map.put("code", e.getCode());
+        map.put("msg", e.getMsg());
+        return map;
+    }
+
+}
