5e3a6e198f935d2094ff68a3fdcfe5efe8785c0b
Lyle22 <302843143@qq.com>
2019-12-23 10:12:03 +0800

    update .gitignore

---
+++ rabbit-parent/system-user/src/main/java/com/rabbit/login/controller/LoginController.java
@@ -0,0 +1,17 @@
+package com.rabbit.login.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * 登录控制器
+ * 
+ * @author geestu
+ *
+ */
+@RestController
+@RequestMapping("/login")
+public class LoginController {
+
+	
+}
